415da6b272a58a10e3899a9b37068db86bd21b89
YouXianMing <705786299@qq.com>
2015-09-18 21:24:35 +0800

    Initial Commit

---
+++ TableViewTapAnimationTests/TableViewTapAnimationTests.m
@@ -0,0 +1,39 @@
+//
+//  TableViewTapAnimationTests.m
+//  TableViewTapAnimationTests
+//
+//  Created by YouXianMing on 15/9/18.
+//  Copyright © 2015年 YouXianMing. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TableViewTapAnimationTests : XCTestCase
+
+@end
+
+@implementation TableViewTapAnimationTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
